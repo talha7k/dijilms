56f3298fa490154a65493a409aa59b35b255bfc4
Talha Khan <talha7k@gmail.com>
2025-10-13 17:24:05 +0300

    fix vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { mdsvex } from "mdsvex";
 
@@ -16,7 +16,9 @@ const config = {
   ],
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      runtime: "nodejs20.x",
+    }),
   },
 };
 
