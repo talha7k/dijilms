c37dd3465aae9ce77a1782ee2db2be12778653e2
Talha Khan <talha7k@gmail.com>
2025-10-13 21:50:58 +0300

    fix side bar

--- src/lib/components/app/nav/app-sidebar.svelte
+++ src/lib/components/app/nav/app-sidebar.svelte
@@ -1,39 +1,48 @@
 <script lang="ts">
-	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import type { ComponentProps } from 'svelte';
-	import { siteConfig } from '../../../../config';
-	import NavMain from './nav-main.svelte';
-	import NavUser from './nav-user.svelte';
-	let {
-		ref = $bindable(null),
-		collapsible = 'icon',
-		...restProps
-	}: ComponentProps<typeof Sidebar.Root> = $props();
+  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+  import type { ComponentProps } from "svelte";
+  import { siteConfig } from "../../../../config";
+  import NavMain from "./nav-main.svelte";
+  import NavUser from "./nav-user.svelte";
+  let {
+    ref = $bindable(null),
+    collapsible = "icon",
+    ...restProps
+  }: ComponentProps<typeof Sidebar.Root> = $props();
 </script>
 
 <Sidebar.Root bind:ref {collapsible} {...restProps}>
-	<Sidebar.Header>
-		<Sidebar.Menu>
-			<Sidebar.MenuItem>
-				<Sidebar.MenuButton
-					size="lg"
-					class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-				>
-					{#snippet child({ props })}
-						<a href="/" class="cursor-pointer" {...props}>
-							<img src={siteConfig.logo} alt={siteConfig.title} class="h-6 dark:hidden" />
-							<img src={siteConfig.logoDark} alt={siteConfig.title} class="hidden h-6 dark:block" />
-						</a>
-					{/snippet}
-				</Sidebar.MenuButton>
-			</Sidebar.MenuItem>
-		</Sidebar.Menu>
-	</Sidebar.Header>
-	<Sidebar.Content>
-		<NavMain />
-	</Sidebar.Content>
-	<Sidebar.Footer>
-		<NavUser />
-	</Sidebar.Footer>
-	<Sidebar.Rail />
+  <Sidebar.Header>
+    <Sidebar.Menu>
+      <Sidebar.MenuItem class="flex items-center">
+        <Sidebar.MenuButton
+          size="lg"
+          class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground flex-grow"
+        >
+          {#snippet child({ props })}
+            <a href="/" class="cursor-pointer" {...props}>
+              <img
+                src={siteConfig.logo}
+                alt={siteConfig.title}
+                class="h-6 dark:hidden"
+              />
+              <img
+                src={siteConfig.logoDark}
+                alt={siteConfig.title}
+                class="hidden h-6 dark:block"
+              />
+            </a>
+          {/snippet}
+        </Sidebar.MenuButton>
+        <Sidebar.Trigger class="group-data-[state=collapsed]:hidden" />
+      </Sidebar.MenuItem>
+    </Sidebar.Menu>
+  </Sidebar.Header>
+  <Sidebar.Content>
+    <NavMain />
+  </Sidebar.Content>
+  <Sidebar.Footer>
+    <NavUser />
+  </Sidebar.Footer>
+  <Sidebar.Rail />
 </Sidebar.Root>
